f9b3d8ac0fdb8b267aea52dc79d4c66de3641893
Nikita Leshenko <nikita.leshenko@gmail.com>
2014-11-01 01:19:53 +0200

    Root stuff

---
+++ scripts/install_root.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Flash
+sed -i 's/\(deb [^ ]* wheezy main\)/\1 contrib/' /etc/apt/sources.list
+echo 'deb http://http.debian.net/debian wheezy-backports main' >> /etc/apt/sources.list
+apt-get -y remove gnash
+apt-get update
+apt-get -y upgrade
+apt-get -y install hal flashplugin-nonfree nodejs
+
+# Hosts
+echo 'local.1tv.ru 127.0.0.1' >> /etc/hosts
