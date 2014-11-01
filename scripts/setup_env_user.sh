8e4ee34b72926b15b7d6fc04c070fa3e621d0e8d
Nikita Leshenko <nikita.leshenko@gmail.com>
2014-11-01 12:51:40 +0200

    Home dir expantion bug

--- scripts/setup_env_user.sh
+++ scripts/setup_env_user.sh
@@ -3,10 +3,10 @@
 set -e
 
 THIS="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-CONFIG="~/.config/autostart"
+CONFIG=~/.config/autostart
 
 mkdir -p $CONFIG
-mv $THIS/1tv.desktop $THIS/xset.desktop $CONFIG
+cp $THIS/1tv.desktop $THIS/xset.desktop $CONFIG
 
 gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
 gsettings set org.gnome.desktop.screensaver lock-enabled false
